f63140e32470aede48fc3ad84600cdecc294f1ed
Kevin Ruffe <kevinjruffe@gmail.com>
2026-03-13 22:15:08 -0400

    added footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,25 @@
+export default function Footer() {
+  return (
+    <footer className="site-footer">
+      <div
+        title="Henceforth I ask not good-fortune, I myself am good-fortune,
+Henceforth I whimper no more, postpone no more, need nothing,
+Done with indoor complaints, libraries, querulous criticisms,
+Strong and content I travel the open road.
+
+The earth, that is sufficient,
+I do not want the constellations any nearer,
+I know they are very well where they are,
+I know they suffice for those who belong to them.
+
+(Still here I carry my old delicious burdens,
+I carry them, men and women, I carry them with me wherever I go,
+I swear it is impossible for me to get rid of them,
+I am fill'd with them, and I will fill them in return.)
+- WW"
+      >
+        Amor Fati
+      </div>
+    </footer>
+  );
+}
