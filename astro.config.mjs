61a9340c1a49f5dd74f7370fcf29877a349767e5
Kevin Ruffe <kevinjruffe@gmail.com>
2026-03-20 23:02:28 -0400

    remove custom fonts to eliminate flash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,9 @@
 // @ts-check
-import { defineConfig, fontProviders } from "astro/config";
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  fonts: [
-    {
-      provider: fontProviders.fontsource(),
-      name: "Newsreader",
-      cssVariable: "--font-newsreader",
-      display: "fallback",
-      weights: [400, 700],
-      styles: ["normal"],
-    },
-    {
-      provider: fontProviders.fontsource(),
-      name: "Fira Code",
-      cssVariable: "--font-fira-code",
-      display: "fallback",
-      weights: [400],
-      styles: ["normal"],
-    },
-  ],
   integrations: [react(), mdx()],
 });
