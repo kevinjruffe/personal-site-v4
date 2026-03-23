c26a6313c1ad3a9d2e9931fe03d34c116e52dc8c
Kevin Ruffe <kevinjruffe@gmail.com>
2026-03-22 21:44:29 -0400

    update browsers

--- playwright.config.js
+++ playwright.config.js
@@ -28,8 +28,16 @@ export default defineConfig({
       use: { ...devices["Desktop Chrome"] },
     },
     {
+      name: "safari",
+      use: { ...devices["Desktop Safari"] },
+    },
+    {
       name: "mobile-chrome",
       use: { ...devices["Pixel 7"] },
+    },
+    {
+      name: "mobile-safari",
+      use: { ...devices["iPhone 15"] },
     },
   ],
 });
